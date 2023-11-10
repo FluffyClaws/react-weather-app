5766c7b28673a9532ade38b469fb0b69d4b14ad0
Anastasia <ana.ko8915@gmail.com>
2023-11-10 12:10:22 +0200

    theme toggle

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,25 +3,29 @@ import WeatherForm from "../components/WeatherForm";
 import WeatherCard from "../components/WeatherCard";
 import LastViewedCities from "../components/LastViewedCities";
 import useWeather from "../hooks/useWeather";
-import { Container } from "@mui/material";
+import { Container, Switch } from "@mui/material";
 import "./App.scss";
+import useTheme from "../hooks/useTheme";
 
 const App: React.FC = () => {
   const { weather, error, isLoading, lastViewedCities, handleCitySubmit } =
     useWeather();
+  const { theme, toggleTheme } = useTheme();
 
   return (
-    <Container className="app-container">
+    <Container className="app-container" data-theme={theme}>
+      <Switch
+        checked={theme === "dark"}
+        onChange={toggleTheme}
+        name="themeToggle"
+        inputProps={{ "aria-label": "theme toggle" }}
+      />
       <WeatherForm onCitySubmit={handleCitySubmit} />
       <LastViewedCities
         onSelectCity={handleCitySubmit}
         lastViewedCities={lastViewedCities}
       />
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : weather ? (
-        <WeatherCard weather={weather} />
-      ) : null}
+      <WeatherCard weather={weather} isLoading={isLoading} />
       {error && <div>Error: {error}</div>}
     </Container>
   );
